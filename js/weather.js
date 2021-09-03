93a79119fbc585e8fa066cf4dc57e24e684b0e64
Arjun Roy <arjunroy.arjun2@gmail.com>
2021-09-03 14:59:59 +0600

    Add clouds and more

--- js/weather.js
+++ js/weather.js
@@ -1,64 +1,80 @@
-// secelt errMsg & emptyMsg field 
-const errMsg = document.getElementById('err-msg');
-const emptyMsg = document.getElementById('empty-msg');
+// secelt errMsg & emptyMsg field
+const notification = (condition1, condition2) => {
+    const errMsg = document.getElementById('err-msg');
+    const emptyMsg = document.getElementById('empty-msg');
+    if(condition1 === false && condition2 === condition2) {
+        emptyMsg.innerHTML =  '';
+        errMsg.innerHTML = `Your city <span class="text-danger display-5">${condition2}</span> is not found in Server.`;
+    }
+    else if (condition1 === true && condition2 === false) {
+        emptyMsg.innerHTML =  '';
+        emptyMsg.innerHTML = 'Your input is empty. Please input your city name.';
+    }
+    else {
+        emptyMsg.innerHTML = '';
+        errMsg.innerHTML = '';
+    }
+}
+
+// call from changeBackground function for change body background
+const backgroundStyle = (imgName) => {
+    const selectBody = document.getElementById('body');
+    selectBody.style.background= `url(../images/${imgName}.jpg) no-repeat`;
+    selectBody.style.transition = 'all 0.9s';
+    selectBody.style.backgroundSize = 'cover';
+    selectBody.style.backgroundPosition = 'center';
+    selectBody.style.height = '100vh';
+}
+
+// call from loadData function for change body background
+changeBackground = () => {
+    const weatherType = document.getElementById('weather-type');
+    const weatherImg = weatherType.innerText.toLowerCase();
+    backgroundStyle(weatherImg);
+}
+
+const loadData = data => {
+    const {clouds, main, name, weather, wind, sys} = data;
+    const weatherIcon = weather[0].icon;
+    const country = sys.country;
+
+    const changeIcon = document.getElementById('change-icon');
+    const cityName = document.getElementById('city-name');
+    const tempeture = document.getElementById('temp');
+    const weatherTypeShow = document.getElementById('weather-type');
+    const feel = document.getElementById('feels');
+    const cloudResult = document.getElementById('clouds');
+    const windSpeed = document.getElementById('wind-speed');
+    
+    changeIcon.src = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+    cityName.innerHTML = `${name}-${country}`;
+    tempeture.innerHTML = main.temp;;
+    weatherTypeShow.innerHTML = weather[0].main;
+    feel.innerHTML = main.feels_like;
+    cloudResult.innerHTML = clouds.all;
+    windSpeed.innerHTML = wind.speed;
+    // change backgroud Image 
+    changeBackground()
+}
 
 // load Data from API & Matching  input value & 
 function weatherResult() {
+    notification ();
     const inputField = document.getElementById('input-value');
     const apiKey = 'dc7b65ba5881074ed1dc9abdf396d72c';
     const inputText = inputField.value;
     inputField.value = '';
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputText}&appid=${apiKey}`;
+    if (inputText === '') {
+        notification (true, false);
+    }
+    else {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputText}&appid=${apiKey}&units=metric`;
         fetch(url)
         .then(res => res.json())
-        .then(data => loadData(data));
-}
-
-const loadData = data => {
-    console.log(data);
-    const {main, name, weather} = data;
-    console.log(weather[0].id);
-
-    const tempCal = main.temp - 275.5;
-    const temp = tempCal.toFixed(2);
-
-    const weatherType = weather[0].main;
-
-    console.log(temp);
-    console.log(weatherType);
-    const cityName = document.getElementById('city-name');
-    cityName.innerHTML = name;
-    const tempeture = document.getElementById('temp');
-    tempeture.innerHTML = temp;
-    const weatherTypeShow = document.getElementById('weather-type');
-    weatherTypeShow.innerHTML = weatherType;
-    changeBackground()
-}
-
-// For changing background with weather type
-changeBackground = () => {
-    const selectBody = document.getElementById('body');
-    const weatherType = document.getElementById('weather-type');
-    const weatherText = weatherType.innerText;
-    console.log(weatherText);
-    if(weatherText === 'Clouds') {
-        selectBody.style.background= 'url(../images/megh.jpg) no-repeat';
-        selectBody.style.backgroundSize = 'cover';
-        selectBody.style.backgroundPosition = 'center';
-        selectBody.style.height = '100vh';
-    }
-
-    else if(weatherText === 'Rain') {
-        selectBody.style.background= 'url(../images/rain.jpg) no-repeat';
-        selectBody.style.backgroundSize = 'cover';
-        selectBody.style.backgroundPosition = 'center';
-        selectBody.style.height = '100vh';
-    }
-
-    else if(weatherText === 'Haze' || weatherText === 'Clear') {
-        selectBody.style.background= 'url(../images/hotsun.jpg) no-repeat';
-        selectBody.style.backgroundSize = 'cover';
-        selectBody.style.backgroundPosition = 'center';
-        selectBody.style.height = '100vh';
+        .then(data => loadData(data))
+        .catch(error => {
+            console.log(error);
+            notification (false, inputText);
+        });
     }
 }
